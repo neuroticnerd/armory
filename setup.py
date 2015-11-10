ad839df5208a2389283f16cdc774eed741441ab7
neuroticnerd <eggleton.bryce@gmail.com>
2015-11-10 17:20:06 -0500

    Cleaned up setup.py file and added __version__.py file

--- setup.py
+++ setup.py
@@ -19,26 +19,23 @@ LONG_DESC = ''
 LICENSE = ''
 VERSION = ''
 
-initfile = '{0}/__init__.py'.format(PROJECT_MODULE)
-with open(initfile, 'r', encoding='utf-8') as modinit:
-    findver = re.compile(r'^\s*__version__\s*=\s*[\"\'](.*)[\"\']', re.M)
-    try:
-        VERSION = findver.search(modinit.read()).group(1)
-    except AttributeError:
-        VERSION = '0.1.0'
+version_file = '{0}/__version__.py'.format(PROJECT_MODULE)
+with open(version_file, 'r', encoding='utf-8') as fver:
+    re_ver = r'^\s*__version__\s*=\s*[\"\'](.*)[\"\']$'
+    VERSION = re.search(re_ver, fver.read(), re.MULTILINE).group(1)
 
 with open('LICENSE', 'r', encoding='utf-8') as f:
-    LICENSE = r.read()
+    LICENSE = f.read()
 
 setup(
-    name = PROJECT,
-    version = VERSION,
-    packages = find_packages(include=[PROJECT_MODULE]),
-    author = AUTHOR,
-    author_email = EMAIL,
-    url = URL,
-    description = DESC,
-    long_description = LONG_DESC,
+    name=PROJECT,
+    version=VERSION,
+    packages=find_packages(include=[PROJECT_MODULE]),
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    description=DESC,
+    long_description=LONG_DESC,
     license=LICENSE,
     install_requires=REQUIRES,
-    )
+)
