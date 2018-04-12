2fc7ec3c0c675b731db65fa1c593f3b23123ff64
Bryce Eggleton <kraken@neuroticnerd.com>
2018-04-12 15:32:18 -0400

    Add glob-style param to click helpers

--- armory/cli/click.py
+++ armory/cli/click.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
+import glob
 import logging
+import os
 import re
 
 from collections import OrderedDict
@@ -59,6 +61,100 @@ class DeltaDateParamType(click.ParamType):
 
 
 DELTA_DATE = DeltaDateParamType()
+
+
+class GlobIterator(object):
+
+    def __init__(self, pattern, abspath=False, files=True, dirs=True):
+        self.obj = glob.glob(pattern)
+        self.abspath = abspath
+        self.files = files
+        self.dirs = dirs
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        """Current method for iteration."""
+        try:
+            item = self.obj[self._index]
+            self._index += 1
+            item_isdir = os.path.isdir(item)
+            if item_isdir and not self.dirs:
+                # TODO: deal with possible stack overflow
+                return self.__next__()
+            if not item_isdir and not self.files:
+                # TODO: deal with possible stack overflow
+                return self.__next__()
+            if self.abspath:
+                item = os.path.abspath(item)
+            return item
+        except IndexError:
+            raise StopIteration()
+
+    def next(self):
+        """Included for backward compatibility."""
+        return self.__next__()
+
+
+class Glob(object):
+
+    def __init__(self, pattern, abspath=False, files=True, dirs=True):
+        self.pattern = pattern
+        if os.path.isdir(self.pattern):
+            self.pattern = os.path.join(self.pattern, '*')
+        self.abspath = abspath
+        self.files = files
+        self.dirs = dirs
+        self._glob = None
+
+    def __iter__(self):
+        return self.glob.__iter__()
+
+    def __len__(self):
+        return len(self.glob)
+
+    @property
+    def glob(self):
+        if self._glob is None:
+            self._glob = []
+            iterargs = (self.pattern, self.abspath, self.files, self.dirs)
+            for path in GlobIterator(*iterargs):
+                self._glob.append(path)
+        return self._glob
+
+    def iglob(self):
+        return glob.iglob(self.pattern)
+
+
+class GlobParamType(click.ParamType):
+    """Converts to a Glob object.
+
+    TODO: look at whether using fnmatch.fnmatch() directly or regex patterns
+    for filename matching is more useful than straight glob iteration!
+
+    The glob module finds all the pathnames matching a specified pattern
+    according to the rules used by the Unix shell, although results are
+    returned in arbitrary order. No tilde expansion is done, but *, ?, and
+    character ranges expressed with [] will be correctly matched. This is
+    done by using the os.scandir() and fnmatch.fnmatch() functions in concert,
+    and not by actually invoking a subshell. Note that unlike
+    fnmatch.fnmatch(), glob treats filenames beginning with a dot (.) as
+    special cases. (For tilde and shell variable expansion,
+    use os.path.expanduser() and os.path.expandvars().)
+    """
+
+    envvar_list_splitter = os.path.pathsep
+
+    def __init__(self, exists=False, readable=True):
+        self.exists = exists
+        self.readable = readable
+        self.name = 'glob'
+        self.ntype = 'Glob'
+
+    def convert(self, value, param, ctx):
+        return Glob(value, abspath=True, dirs=False)
 
 
 class InvocationContext(object):
