87291dd8107f067433350edb8264c21bbb83ab66
Bryce Eggleton <kraken@neuroticnerd.com>
2018-04-11 22:23:41 -0400

    Add iteration helper utilities

---
+++ armory/itertools.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, unicode_literals
+
+import itertools
+import random
+
+
+def iterchunks(iterable_obj, chunk_size, chunk_type=tuple):
+    source_iter = iter(iterable_obj)
+    chunk = chunk_type(itertools.islice(source_iter, chunk_size))
+    while chunk:
+        yield chunk
+        chunk = chunk_type(itertools.islice(source_iter, chunk_size))
+
+
+def rand_iterchunks(iterable_obj, chunk_min, chunk_max, chunk_type=tuple):
+    source_iter = iter(iterable_obj)
+    chunk = chunk_type(itertools.islice(source_iter, random.randint(chunk_min, chunk_max)))
+    while chunk:
+        yield chunk
+        chunk = chunk_type(itertools.islice(source_iter, random.randint(chunk_min, chunk_max)))
