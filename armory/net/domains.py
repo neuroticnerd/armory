64a53c74f1d19ee3cced2f423f2ef9367e276055
Bryce Eggleton <kraken@neuroticnerd.com>
2018-04-13 02:45:21 -0400

    Improve domain parser to recognize CCTLD domains

--- armory/net/domains.py
+++ armory/net/domains.py
@@ -1,8 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
 import logging
-
-from sortedcontainers import SortedDict
+from collections import OrderedDict
 
 # We use this to retrieve the list of top level domains we want
 # http://stackoverflow.com/questions/14406300/python-urlparse-extract-domain-name-without-subdomain#answer-22228140
@@ -13,17 +12,8 @@ from .exceptions import ArmoryError
 log = logging.getLogger(__name__)
 
 
-DEFAULT_ENCODING_LIST = (
-    'ISO-8859-2',
-    'ISO-8859-1',
-    'utf-8',
-    'windows-1250',
-    'windows-1252',
-    'utf-16',
-)
-DEFAULT_ENCODING = 'utf-8'
-DEFAULT_DELIMITER = ','
 TLD_LOOKUP_MAPPING = None
+IDN_CCTLD_PREFIX = 'xn--'
 
 
 class InvalidDomainError(ArmoryError):
@@ -42,26 +32,18 @@ def init_tld_mapping(update=True, use_sorted=False):
     if update:
         tld.utils.update_tld_names()
 
-    # TODO: is the SortedDict actually faster at all?
-    def get_dict():
-        if use_sorted:
-            result = SortedDict()
-        else:
-            result = dict()
-        return result
-
-    TLD_LOOKUP_MAPPING = get_dict()
+    TLD_LOOKUP_MAPPING = OrderedDict()
 
     for domain in tld.get_tld_names():
         current = TLD_LOOKUP_MAPPING
         for part in reversed(domain.split('.')):
-            current.setdefault(part, get_dict())
+            current.setdefault(part, OrderedDict())
             current = current[part]
 
     log.debug('done.')
 
 
-def get_root_domain(domain):
+def get_root_domain(domain, raises=False):
     """
     Extract the second level domain + TLD from a given domain string.
 
@@ -75,22 +57,44 @@ def get_root_domain(domain):
     https://moz.com/blog/understanding-root-domains-subdomains-vs-subfolders-microsites
     https://en.wikipedia.org/wiki/Second-level_domain
     """
-    root = []
+    global IDN_CCTLD_PREFIX
+    global TLD_LOOKUP_MAPPING
     lookup = TLD_LOOKUP_MAPPING
-    base_check = True
-    domain_parts = reversed(domain.split('.'))
-    for part in domain_parts:
-        lookup = lookup.get(part, None)
-        if base_check:
+    length = len
+
+    do_base_check = True
+    domain_len = length(domain)
+    sld_index = 0
+    for part in reversed(domain.split('.')):
+        # lookup = lookup.get(part, None) is slower than this way
+        if part in lookup:
+            lookup = lookup[part]
+        else:
+            lookup = None
+
+        if do_base_check:
             if lookup is None:
-                raise InvalidDomainError('TLD not found: "{0}"'.format(part))
-            base_check = False
-        root.insert(0, part)
+                if part.startswith(IDN_CCTLD_PREFIX):
+                    # this is a hacky way to deal with them being commented
+                    # out in the publicsuffix.org source file
+                    TLD_LOOKUP_MAPPING.setdefault(part, OrderedDict())
+                    lookup = TLD_LOOKUP_MAPPING[part]
+                elif raises:
+                    error_msg = 'TLD \'{0}\' not found from {1}'
+                    raise InvalidDomainError(error_msg.format(part, repr(domain)))
+                else:
+                    return None
+            do_base_check = False
+
+        sld_index += (length(part) + 1)
         if lookup is None:
             break
 
-    result = '.'.join(root)
-    return result
+    sld_index = (domain_len - sld_index + 1)
+    if sld_index < 0:
+        sld_index = None
+
+    return domain[sld_index:]
 
 
 get_second_level_domain = get_root_domain
